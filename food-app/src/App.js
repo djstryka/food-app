5d95daed87d063bb9c7a92ce0bbcb7a375cc52db
djstryka <fredmireku2020@gmail.com>
2024-01-29 11:39:53 +0000

    New Food App

---
+++ food-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/navigation bar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
